a522c5712777c71406d0caa15ad2e8282a3a77f7
kentilove095 <6210110616@psu.ac.th>
2021-07-05 11:22:57 +0700

    ninth commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css'
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
 const word = "Hello";
 function App() {
   return (
     <div>
       {
-      Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-      }
+          <WordCard value={word}/>
+      }   
     </div>
   );
 }
